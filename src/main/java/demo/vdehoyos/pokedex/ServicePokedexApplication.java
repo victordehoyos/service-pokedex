6bc9a611174dbe6b9b5fd1cd8125bc84b7a20f4c
victordehoyos <50594834+victordehoyos@users.noreply.github.com>
2022-11-23 23:45:06 -0500

    Commit Base

---
+++ src/main/java/demo/vdehoyos/pokedex/ServicePokedexApplication.java
@@ -0,0 +1,13 @@
+package demo.vdehoyos.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePokedexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/vdehoyos/pokedex/ServicePokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.vdehoyos.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
